8d117f7b5fe06c3e7c13393a70330500ab4936ab
Kanwar Ujjaval Singh <ujjavalkanwar@gmail.com>
2014-03-02 16:48:12 +0530

    anglar routing init

--- core/routes/publicRoutes.js
+++ core/routes/publicRoutes.js
@@ -1,8 +1,12 @@
 module.exports = function (app) {
+
+    
+    app.get('/partials/*', function (req, res) {
+        res.render('../views/' + req.params);
+    });
+
     app.get('*', function (req, res) {
-        res.render('index', {
-            title: "Home Page"
-        });
+        res.render('index');
     });
 
 }
