47ecb57de9ad6c002fdb0306c23879956821d55e
Kanwar Ujjaval Singh <ujjavalkanwar@gmail.com>
2014-07-24 17:20:26 +0530

    added /health for test

--- core/routes/setRoutes.js
+++ core/routes/setRoutes.js
@@ -5,6 +5,11 @@ var user = require('../api/user');
 var lect = require('../api/lectures');
 
 module.exports = function (app) {
+
+    app.get('/health', function (req, res) {
+        res.send("Running on " + ipaddress + "\nenvironement = " + env+ "\ndatabase at " + require('../config/config.js').dbConnection[env].uri)
+    });
+
     /*
     Course routes
     */
