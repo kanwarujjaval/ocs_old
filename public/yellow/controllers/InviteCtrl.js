492efd25c2fd41d7f91a4104d36fd8a8ec34fcbf
Sachin Verma <sachinvermarip@gmail.com>
2014-07-26 17:44:23 +0530

    Styled popup, added facebook opengraph meta tags(temporary till beta).

--- public/yellow/controllers/InviteCtrl.js
+++ public/yellow/controllers/InviteCtrl.js
@@ -26,7 +26,7 @@ yellow.controller('InsideCtrl', function ($scope, ngDialog) {
     $scope.openSecond = function () {
         ngDialog.close('ngdialog1');
         ngDialog.open({
-            template: '<h3><a href="" ng-click="closeSecond()">Click here to close</a></h3>',
+            template: '<h3>Share with your friends!</h3><a href="http://www.facebook.com/share.php?u=http://www.opencloudschool.org&title=I just signed up for Open Cloud School beta" target="_blank"><button class="btn-flat btn-fb">Share</button></a><a href="http://twitter.com/intent/tweet?url=http://www.opencloudschool.org/&text=I+just+signed+up+for+Open+Cloud+School+beta!&hashtags=OpenCloudSchool,Beta" target="_blank"><button class="btn-flat btn-twitter">Tweet</button></a>',
             plain: true,
             closeByEscape: false,
             controller: 'SecondModalCtrl'
