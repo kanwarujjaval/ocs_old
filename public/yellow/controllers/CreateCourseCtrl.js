2d75dd2c4d02fee3aec77571e2357a42258e1894
Kanwar Ujjaval Singh <ujjavalkanwar@gmail.com>
2014-10-24 22:41:34 +0530

    minor fix to upload

--- public/yellow/controllers/CreateCourseCtrl.js
+++ public/yellow/controllers/CreateCourseCtrl.js
@@ -44,33 +44,30 @@
                 )
     }
 
-
     $scope.onFileSelect = function ($files) {
-        //$files: an array of files selected, each file has name, size, and type.
         for (var i = 0; i < $files.length; i++) {
             var file = $files[i];
-            $scope.upload = $upload.upload({
-                url: 'server/upload/url', //upload.php script, node.js route, or servlet url
-                //method: 'POST' or 'PUT',
-                //headers: {'header-key': 'header-value'},
-                //withCredentials: true,
-                data: { myObj: $scope.myModelObj },
-                file: file, // or list of files ($files) for html5 only
-                //fileName: 'doc.jpg' or ['1.jpg', '2.jpg', ...] // to modify the name of the file(s)
-                // customize file formData name ('Content-Disposition'), server side file variable name. 
-                //fileFormDataName: myFile, //or a list of names for multiple files (html5). Default is 'file' 
-                // customize how data is added to formData. See #40#issuecomment-28612000 for sample code
-                //formDataAppender: function(formData, key, val){}
-            }).progress(function (evt) {
-                console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
-            }).success(function (data, status, headers, config) {
-                // file is uploaded successfully
-                console.log(data);
-            });
-            //.error(...)
-            //.then(success, error, progress); 
-            // access or attach event listeners to the underlying XMLHttpRequest.
-            //.xhr(function(xhr){xhr.upload.addEventListener(...)})
+            apiService.get('/api/course/module/genuptoken').then(
+                   function (response) {
+                       $scope.upload = $upload.upload({
+                           url: response.upload_link_secure,
+                           method: 'POST',
+                           //headers: {'header-key': 'header-value'},
+                           //withCredentials: true,
+                           //data: { myObj: $scope.myModelObj },
+                           file: file,
+                       }).progress(function (evt) {
+                           console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
+                       }).success(function (data, status, headers, config) {
+                           console.log(status)
+                           console.log(data);
+                       });
+                       //.error(...)
+                       //.then(success, error, progress); 
+                       // access or attach event listeners to the underlying XMLHttpRequest.
+                       //.xhr(function(xhr){xhr.upload.addEventListener(...)})
+                   }
+                )
         }
     };
 });
